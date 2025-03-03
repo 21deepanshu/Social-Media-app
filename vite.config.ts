bd3f170c4902b8d0c0f7078d2a2dd16b30da9ab3
21deepanshu <90502741+21deepanshu@users.noreply.github.com>
2025-03-03 09:40:21 +0530

    deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   ssr: {
     noExternal: ['@mui/material', '@mui/system', '@mui/icons-material', '@mui/styled-engine'],
   },
   build: {
     outDir: 'dist',
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
   },
 });
